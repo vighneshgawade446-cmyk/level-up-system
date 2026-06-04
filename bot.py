c5a7dfbe53f879e172c0593c4886338070867e0d
vighneshgawade446-cmyk <vighneshgawade446@gmail.com>
2026-06-04 12:50:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = 8888125390: AAFRevhLGSYGGf4 fm64mg
-PoE4FL39uQ9QtM
+TOKEN = "8888125390: AAFRevhLGSYGGf4 fm64mg
+PoE4FL39uQ9QtM"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(

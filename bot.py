8f94549b77c76f2ee6a8b13ada2a5c7bc269387b
vighneshgawade446-cmyk <vighneshgawade446@gmail.com>
2026-06-04 12:29:08 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = 8888125390: AAFRevhLGSYGGf4 fm64mg
+PoE4FL39uQ9QtM
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "⚔️ Welcome to Level Up System!\n\nHunter registration will be added soon."
+    )
+
+async def profile(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "👤 Hunter Profile\n\nRank: E\nLevel: 1\nGold: 0"
+    )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("profile", profile))
+
+    print("Bot is running...")
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
